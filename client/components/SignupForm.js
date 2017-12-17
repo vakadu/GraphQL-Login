5e023e676722be7adb5c7615c7e91f573aa1bf2d
vinod10 <vinodkumar@kwiqpick.com>
2017-12-17 17:38:56 +0530

    added HOC for dashboard component

---
+++ client/components/SignupForm.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import { graphql } from 'react-apollo';
+import { hashHistory } from 'react-router';
+
+import AuthForm from './AuthForm';
+import mutation from '../mutations/signup';
+import query from '../queries/CurrentUser';
+
+class SignupForm extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = { errors: [] }
+    }
+
+    componentWillUpdate(nextProps){
+        if (nextProps.data.user && !this.props.data.user){
+            hashHistory.push("/dashboard");
+        }
+    }
+
+    onSubmit({email, password}){
+        this.props.mutate({
+            variables: {email, password},
+            refetchQueries: [{ query }]
+        }).catch(res => {
+            const errors = res.graphQLErrors.map(error => error.message);
+            this.setState({ errors });
+        });
+    }
+
+    render(){
+        return(
+            <div>
+                <h3>Signup</h3>
+                <AuthForm
+                    errors={this.state.errors}
+                    onSubmit={this.onSubmit.bind(this)} />
+            </div>
+        );
+    }
+}
+
+export default graphql(query)(
+    graphql(mutation) (SignupForm)
+);
--- server/schema/types/user-type.js
+++ server/schema/types/user-type.js
@@ -2,12 +2,14 @@ const graphql = require('graphql');
 
 const {
     GraphQLObjectType,
+    GraphQLID,
     GraphQLString
 } = graphql;
 
 const UserType = new GraphQLObjectType({
     name: 'UserType',
     fields: {
+        id: { type: GraphQLID },
         email: { type: GraphQLString }
     }
 });
